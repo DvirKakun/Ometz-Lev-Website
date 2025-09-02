7821b0c5a3eae3d2c1b21049ff6dbe5780a4fa77
dvirkakun <dvireteui1@gmail.com>
2025-09-02 11:47:01 +0300

    Create the tiktok icon with animation while hovering

--- src/components/common/TikTokIcon.tsx
+++ src/components/common/TikTokIcon.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { ReactComponent as TikTokSvg } from "../../assets/icons/tiktok.svg";
+import { ReactComponent as TikTokBlancSvg } from "../../assets/icons/Tiktok-blanc.svg";
+import { ReactComponent as TikTokColorfulSvg } from "../../assets/icons/Tiktok-colorful.svg";
 
 interface TikTokIconProps {
   className?: string;
 }
 
 const TikTokIcon: React.FC<TikTokIconProps> = ({ className = "w-5 h-5" }) => {
-  return <TikTokSvg className={className} />;
+  return (
+    <div className={`${className} relative`}>
+      <TikTokBlancSvg className="w-full h-full absolute transition-opacity duration-300 group-hover:opacity-0" />
+      <TikTokColorfulSvg className="w-full h-full absolute transition-opacity duration-300 opacity-0 group-hover:opacity-100" />
+    </div>
+  );
 };
 
 export default TikTokIcon;
