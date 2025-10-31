758955b7dca11d82045185126ff3620bc98d82e4
dvirkakun <dvireteui1@gmail.com>
2025-10-31 12:51:32 +0200

    Fix build errors

--- src/components/modals/contact/ContactModalSuccess.tsx
+++ src/components/modals/contact/ContactModalSuccess.tsx
@@ -3,8 +3,14 @@ import { ModalSuccess } from "../shared";
 const ContactModalSuccess = () => {
   return (
     <ModalSuccess
-      title="הודעה נשלחה בהצלחה!"
-      content="נחזור אליכם בהקדם האפשרי"
+      content={
+        <div className="text-center">
+          <h3 className="text-lg font-semibold text-slate-900 mb-2">
+            הודעה נשלחה בהצלחה!
+          </h3>
+          <p className="text-slate-700">נחזור אליכם בהקדם האפשרי</p>
+        </div>
+      }
       iconMarginBottom="mb-3"
       contentAlignment="text-center"
     />
